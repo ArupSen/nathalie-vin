a5865adc49876f7423b1b6b7cdd8d9a2751c4d17
Arup Sen <arupsen48@gmail.com>
2015-02-07 22:28:50 +0000

    added images for carousel and required divs

--- index.php
+++ index.php
@@ -6,6 +6,32 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
+<div id="wrapper"> <!-- cool carousel -->
+<div id="carousel">
+<?php
+img_tag('1.jpg');
+?>
+<?php
+img_tag('2.jpg');
+?>
+<?php
+img_tag('3.jpg');
+?>
+<?php
+img_tag('4.jpg');
+?>
+<?php
+img_tag('5.jpg');
+?>
+
+</div> <!-- end of #carousel -->
+  <a href="#" id="prev" title="Show previous"> </a>
+    <a href="#" id="next" title="Show next"> </a>
+
+</div> <!-- end of #wrapper -->
+
+
+
 
 
 <?php footer(); ?>
