41040f698dffe1dbd173c890bd36c16e5a89f370
Arup Sen <arupsen48@gmail.com>
2015-02-07 11:35:00 +0000

    created empty files

---
+++ exhibitions.php
@@ -0,0 +1,13 @@
+<?php
+// exhibitions.php
+
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+navbar();
+
+// the unique contents will go here up to the footer
+?>
+
+
+
+<?php footer(); ?>
