c78946da3ae511eeba4dad0b3855fc12f4f18907
Arup Sen <arupsen48@gmail.com>
2015-02-07 22:28:09 +0000

    added basic carousel settings from m & m site

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 // Site specific js code
 (function(){
   'use strict';
+	jQuery('#carousel').carouFredSel({
+				width: '100%',
+				items: {visible: 3,
+					start: 0 
+				},
+				scroll: {
+					items: 1,
+					duration: 600,
+					timeoutDuration: 3000
+				},
+				prev: '#prev',
+				next: '#next'
+			});
 }());
