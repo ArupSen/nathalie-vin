d30c10182e12b73f508f20f591730b095bae4d38
Arup Sen <arupsen48@gmail.com>
2015-02-10 12:11:27 +0000

    added all the links and a bootstrap class

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,13 @@
 <?php
 // -- navbar.php --
 ?>
-<ul id="navbar">
-	<li id="nav-home"><a href="index.php" title="home">home</a></li>
-	<li id="nav-about"><a href="about.php" title="about">about</a></li>
-	<li id="nav-contact"><a href="contact.php" title="contact">contact</a></li>
+<ul id="navbar" class="nav nav-justified">
+  <li id="nav-home"><a href="index.php" title="home">home</a></li>
+  <li id="nav-works"><a href="works.php" title="works">works</a></li>
+  <li id="nav-about"><a href="about.php" title="about">about</a></li>
+  <li id="nav-exhibitions"><a href="exhibitions.php" title="exhibitions & spaces">exhibitions & spaces</a></li>
+  <li id="nav-shop"><a href="" title="shop">shop</a></li>
+  <li id="nav-contact"><a href="contact.php" title="contact">contact</a></li>
 </ul>
 <div id="clear"></div>
 <div id="contents">
