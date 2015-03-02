f67851297af762b03aedeee01386f197f8b00a7c
Arup Sen <arupsen48@gmail.com>
2015-03-02 16:42:45 +0000

    set works link to point to fine_art

--- includes/navbar.php
+++ includes/navbar.php
@@ -3,7 +3,7 @@
 ?>
 <ul id="navbar" class="nav nav-justified">
   <li id="nav-home"><a href="index.php" title="home">home</a></li>
-  <li id="nav-works"><a href="works.php" title="works">works</a></li>
+  <li id="nav-works"><a href="fine_art.php" title="works">works</a></li>
   <li id="nav-about"><a href="about.php" title="about">about</a></li>
   <li id="nav-exhibitions"><a href="exhibitions.php" title="exhibitions & spaces">exhibitions & spaces</a></li>
   <li id="nav-shop"><a href="" title="shop">shop</a></li>
