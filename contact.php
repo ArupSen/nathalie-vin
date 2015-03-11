15da1993350624fbf87945b0ab94ad3c417a7798
Arup Sen <arupsen48@gmail.com>
2015-03-11 19:36:04 +0000

    added contents

--- contact.php
+++ contact.php
@@ -6,6 +6,25 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
-
-
+<div class="row">
+<div class="container">
+<?php
+img_tag('contact-asset.jpg');
+?>
+  <div class="contact-info">
+    <h2>CONTACT</h2>
+    For any queries please do not hesitate to contact me<br>
+    (m) 078 4030 3103<br>
+    (w) 020 8 690 00 14<br>
+    (e) <a href="mailto:tempovision68@gmail.com" title="send me an email">tempovision68@gmail.com</a>
+      <div class="social-icons">
+      <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+      <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+      <a href="#" target="_blank"><i class="fa fa-instagram"></i></a>
+      <a href="#" target="_blank"><i class="fa fa-wordpress"></i></a>
+      <a href="#" target="_blank"><i class="fa fa-pinterest"></i></a>
+      </div>
+  </div>
+</div>
+</div>
 <?php footer(); ?>
