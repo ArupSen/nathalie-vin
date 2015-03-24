41047f1d864bfcbfaa67ad6a2d62eb9e958f024a
Arup Sen <arup@arupdesigns.co.uk>
2015-03-24 07:16:35 +0000

    removed '& spaces' from main nav

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,7 +5,7 @@
   <li id="nav-home"><a href="index.php" title="home">home</a></li>
   <li id="nav-works"><a href="fine_art.php" title="works">works</a></li>
   <li id="nav-about"><a href="about.php" title="about">about</a></li>
-  <li id="nav-exhibitions"><a href="exhibitions.php" title="exhibitions & spaces">exhibitions & spaces</a></li>
+  <li id="nav-exhibitions"><a href="exhibitions.php" title="exhibitions & spaces">exhibitions</a></li>
   <li id="nav-shop"><a href="" title="shop">shop</a></li>
   <li id="nav-contact"><a href="contact.php" title="contact">contact</a></li>
 </ul>
