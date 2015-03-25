2ae7f5ff404c1d60965185f9d41742ff9b8f3e06
Arup Sen <arupsen48@gmail.com>
2015-03-25 00:15:26 +0000

    made works into a dropdown menu

--- includes/navbar.php
+++ includes/navbar.php
@@ -3,7 +3,13 @@
 ?>
 <ul id="navbar" class="nav nav-justified">
   <li id="nav-home"><a href="index.php" title="home">home</a></li>
-  <li id="nav-works"><a href="fine_art.php" title="works">works</a></li>
+  <li id="nav-works" class="dropdown">
+<a href="#" title="works" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">works <span class="caret"></span></a>
+  <ul class="dropdown-menu" role="menu">
+      <li><a href="fine_art.php">Fine Art</a></li>
+    <li><a href="commissions.php">Commissions</a></li>
+  </ul>
+</li>
   <li id="nav-about"><a href="about.php" title="about">about</a></li>
   <li id="nav-exhibitions"><a href="exhibitions.php" title="exhibitions & spaces">exhibitions</a></li>
   <li id="nav-shop"><a href="" title="shop">shop</a></li>
