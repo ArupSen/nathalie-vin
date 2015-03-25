452b80c79b7e9bb5b7bd610b5d66a32be70d431d
Arup Sen <arupsen48@gmail.com>
2015-03-25 00:17:18 +0000

    updated using the fine_art page and commissions info

--- commissions.php
+++ commissions.php
@@ -7,12 +7,23 @@ navbar();
 
 // the unique contents will go here up to the footer
 $thumbs = array(
+  // page to link to, image thumbnail, hover text
+  array('bermondsey_eco_garden.php','bermondsey_eco_garden.jpg','bermondsey eco garden <br> 2010'),
+  array('the_earth_is_scratching_herself.php','the_earth_is_scratching_herself.jpg','the earth is scratching herself <br> 2010'),
+  array('the_limehouse_fountain_project.php','the_limehouse_fountain_project.jpg','the limehouse fountain project <br> 2013'),
+  array('waterline.php','waterline.jpg','waterline <br> 2010')
 );
-for ($i = 0; $i < 5; $i++) {
-  echo '<div class="thumb">';
-    img_tag($thumbs[$i]);
-  echo '</div>';
+echo '<div class="row">';
+echo '<div class="container">';
+$thumb_count = count($thumbs);
+for ($i = 0; $i < $thumb_count; $i++) {
+  echo '<a class="thumbnail pull-left col-sm-4 col-md-4 col-xs-12" href="'.$thumbs[$i][0].'">';
+    img_tag($thumbs[$i][1]);
+  echo '<span>'.$thumbs[$i][2].'</span>';
+  echo '</a>';
 }
+echo '</div>';
+echo '</div>';
 ?>
 
 
