2aa8bdc09c35ce66c9c8326d2b120d717a049203
Arup Sen <arupsen48@gmail.com>
2015-04-25 15:47:39 +0100

    wrapped contact info text in tags with classes to enable styling

--- contact.php
+++ contact.php
@@ -14,10 +14,10 @@ img_tag('contact-asset.jpg');
   <div class="contact-info col-sm-6 col-md-6">
     <h2>CONTACT</h2>
     <hr>
-    For any queries please do not hesitate to contact me<br>
-    (m) 078 4030 3103<br>
-    (w) 020 8 690 00 14<br>
-    (e) <a href="mailto:tempovision68@gmail.com" title="send me an email">tempovision68@gmail.com</a>
+ <h3>   For any queries please do not hesitate to contact me</h3>
+    <span class="mobile">(m) 078 4030 3103</span><br>
+    <span class="work">(w) 020 8 690 00 14</span><br>
+    <span class="email">(e) <a href="mailto:tempovision68@gmail.com" title="send me an email">tempovision68@gmail.com</a></span>
       <div class="social-icons">
       <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
       <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
