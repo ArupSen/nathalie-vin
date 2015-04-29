ac047e90b9e59c0372ba7399f98d201b54473cb2
Arup Sen <arupsen48@gmail.com>
2015-04-29 08:55:10 +0100

    added hr under nav and set home page carousel to static

--- js/main.js
+++ js/main.js
@@ -1,18 +1,34 @@
 // Site specific js code
 (function(){
   'use strict';
-	jQuery("#carousel").carouFredSel({
-    width: "100%",
-    items: 3,
-    height: 488,
-    auto: false,
-    scroll: {
-      items: 1,
-    duration: 600
-    },
-    prev: "#prev",
-    next: "#next"
+  jQuery('#carousel').carouFredSel({
+    width: '100%',
+      items: {
+        visible: 3,
+          start: 0
+      },
+      scroll: {
+        items: 1,
+          duration: 600,
+          timeoutDuration: 3000
+      },
+      prev: '#prev',
+      next: '#next',
+      height: 488,
+      auto: false
   });
+	//jQuery("#carousel").carouFredSel({
+    //width: "100%",
+    //items: 3,
+    //height: 488,
+    //auto: false,
+    //scroll: {
+      //items: 1,
+    //duration: 600
+    //},
+    //prev: "#prev",
+    //next: "#next"
+  //});
   // hovering over the big thumbnails on works pages
   $('a.thumbnail').on('mouseenter', function() {
     $(this).find('span').show();
@@ -22,4 +38,6 @@
     $(this).find('span').hide();
     $(this).find('img').css({"opacity":1, "border":"none"});
   });
+  // add horizontal rules under nav elements
+  $('ul#navbar li').append('<hr>');
 }());
