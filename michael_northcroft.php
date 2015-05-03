c2957e6fa591a18ae5e6821b19280e52079e13c0
Arup Sen <arupsen48@gmail.com>
2015-05-03 14:36:45 +0100

    created Michael Northcroft page with images

---
+++ michael_northcroft.php
@@ -0,0 +1,80 @@
+<?php
+// michael_northcroft.php
+
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+navbar();
+
+// the unique contents will go here up to the footer
+?>
+<div id="carousel-nathalie" class="carousel slide col-lg-offset-1" data-ride="carousel">
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+<?php
+img_tag('
+Michael-Northcroft---commission---floor-and-fresco2.jpg
+');
+?>
+</div>
+    <div class="item">
+<?php
+img_tag('
+Michael-Northcroft---Entrance-door-floor.jpg
+');
+?>
+</div>
+    <div class="item">
+<?php
+img_tag('
+Michael-Northcroft---Entrance-floor-behind-door.jpg
+');
+?>
+</div>
+    <div class="item">
+<?php
+img_tag('
+Michael-Northcroft---Entrance-door---top-fresco1.jpg
+');
+?>
+</div>
+    <div class="item">
+<?php
+img_tag('
+Michael-Northcroft---Close-up-floor-2.jpg
+');
+?>
+</div>
+    <div class="item">
+<?php
+img_tag('
+Michael-Northcroft---Close-up-of-the-floor1.jpg
+');
+?>
+</div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-nathalie" role="button" data-slide="prev">
+    <span class="fa fa-caret-left fa-2x" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-nathalie" role="button" data-slide="next">
+    <span class="fa fa-caret-right fa-2x" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div class="container description">
+  <h2>MICHAEL NORTHCROFT</h2>
+  <h3>2014</h3>
+
+<p>Commissioned to create a floor and a fresco for this high-end furniture warehouse in London – 2014
+<br><br>
+4 sqm
+<br><br>
+<a href="http://www.michaelnorthcroft.com" target="_blank">www.michaelnorthcroft.com</a>
+</p>
+</div>
+
+<?php footer(); ?>
